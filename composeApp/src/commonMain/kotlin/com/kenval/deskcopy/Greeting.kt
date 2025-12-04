f2d7b21f42c572a94544f24c6560b39c70791008
Kenan Valjevčić <kenan.valjevcic@gmail.com>
2025-12-04 21:46:56 +0100

    Setup base main app functionality

--- composeApp/src/commonMain/kotlin/com/kenval/deskcopy/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.kenval.deskcopy
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
---
+++ composeApp/src/commonMain/kotlin/com/kenval/deskcopy/theme/DeskCopyTheme.kt
@@ -0,0 +1,20 @@
+package com.kenval.deskcopy.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DeskCopyTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme(),
+        shapes = MaterialTheme.shapes,
+        typography = MaterialTheme.typography,
+        content = content
+    )
+}
